c7098c9d2c3c54ba41ae4b5d91b853c9c18609f4
baibiaowang <77394272+baibiaowang@users.noreply.github.com>
2026-09-23 08:49:22 +0800

    fix 2.0: load generic core as browser scripts

--- www/core/cell-model.js
+++ www/core/cell-model.js
@@ -1,10 +1,1 @@
-export class CellModel {
-  constructor({ columnId, value }) {
-    this.columnId = columnId;
-    this.value = value;
-  }
-}
-
-export function createCell(columnId, value) {
-  return new CellModel({ columnId, value });
-}
+(function(g){class CellModel{constructor({columnId,value='',valueId=''}={}){this.columnId=columnId;this.value=value;this.valueId=valueId}}g.CellModel=CellModel;})(window);
