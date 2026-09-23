04918cd7217d0c5543c284952f65fa52e493ab03
baibiaowang <77394272+baibiaowang@users.noreply.github.com>
2026-09-23 10:54:28 +0800

    发布 2.1.2：更新运行时版本与 APK 地址

--- www/core/app-runtime.js
+++ www/core/app-runtime.js
@@ -2,14 +2,14 @@
 'use strict';
 
 const APP={
-  version:'2.1.1',
-  versionCode:2101,
+  version:'2.1.2',
+  versionCode:2102,
   defaultSource:'https://stocks-txt-file.app.workbuddy.host/stocks.txt',
   updateSources:[
     'https://raw.githubusercontent.com/baibiaowang/GPT/main/update.json',
     'https://cdn.jsdelivr.net/gh/baibiaowang/GPT@main/update.json'
   ],
-  releaseApk:'https://github.com/baibiaowang/GPT/releases/download/v2.1.1/app.apk'
+  releaseApk:'https://github.com/baibiaowang/GPT/releases/download/v2.1.2/app.apk'
 };
 
 const LS={
