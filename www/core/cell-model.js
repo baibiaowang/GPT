9e3ebe159724cdc029d58eba6eab4b0dd9750a26
baibiaowang <77394272+baibiaowang@users.noreply.github.com>
2026-09-23 09:03:53 +0800

    cell model: add value type and stable value id

--- www/core/cell-model.js
+++ www/core/cell-model.js
@@ -1,1 +1,1 @@
-(function(g){class CellModel{constructor({columnId,value='',valueId=''}={}){this.columnId=columnId;this.value=value;this.valueId=valueId}}g.CellModel=CellModel;})(window);
+(function(g){class CellModel{constructor({columnId,value='',valueId='',type='text'}={}){this.columnId=String(columnId||'');this.value=value==null?'':value;this.valueId=String(valueId||'');this.type=String(type||'text')}}g.CellModel=CellModel;})(window);
